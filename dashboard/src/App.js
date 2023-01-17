30470aa22a4f500510d3573d90546c434e1d24bc
ahmedirheem <ahmedirheem2003@gmail.com>
2023-01-17 18:43:30 +0200

    Home Page

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -42,35 +42,35 @@ function App() {
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
               <Navbar />
             </div>
+
+              <Routes>
+                {/* Dashboard */}
+                <Route path='/' element={<Ecommerce/>}/>
+                <Route path='/ecommerce' element={<Ecommerce/>}/>
+
+                {/* Pages */}
+                <Route path='/orders' element={<Orders/>}/>
+                <Route path='/employees' element={<Employees/>}/>
+                <Route path='/customers' element={<Customers/>}/>
+                
+                {/* Apps */}
+                <Route path='/calender' element={<Calendar/>}/>
+                <Route path='/kanban' element={<Kanban/>}/>
+                <Route path='/editor' element={<Editor/>}/>
+                <Route path='/color-piker' element={<ColorPicker/>}/>
+                
+                {/* Charts */}
+                <Route path='/line' element={<Line/>}/>
+                <Route path='/area' element={<Area/>}/>
+                <Route path='/bar' element={<Bar/>}/>
+                <Route path='/pie' element={<Pie/>}/>
+                <Route path='/financial' element={<Financial/>}/>
+                <Route path='/color-mapping' element={<ColorMapping/>}/>
+                <Route path='/stacked' element={<Stacked />}/>
+                
+              </Routes>
+            </div>
           </div>
-
-          <Routes>
-            {/* Dashboard */}
-            <Route path='/' element={<Ecommerce/>}/>
-            <Route path='/ecommerce' element={<Ecommerce/>}/>
-
-            {/* Pages */}
-            <Route path='/orders' element={<Orders/>}/>
-            <Route path='/employees' element={<Employees/>}/>
-            <Route path='/customers' element={<Customers/>}/>
-            
-            {/* Apps */}
-            <Route path='/calender' element={<Calendar/>}/>
-            <Route path='/kanban' element={<Kanban/>}/>
-            <Route path='/editor' element={<Editor/>}/>
-            <Route path='/color-piker' element={<ColorPicker/>}/>
-            
-            {/* Charts */}
-            <Route path='/line' element={<Line/>}/>
-            <Route path='/area' element={<Area/>}/>
-            <Route path='/bar' element={<Bar/>}/>
-            <Route path='/pie' element={<Pie/>}/>
-            <Route path='/financial' element={<Financial/>}/>
-            <Route path='/color-mapping' element={<ColorMapping/>}/>
-            <Route path='/stacked' element={<Stacked />}/>
-            
-          </Routes>
-        </div>
       </BrowserRouter>
     </div>
   );
--- dashboard/src/components/Charts/SparkLine.jsx
+++ dashboard/src/components/Charts/SparkLine.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts'
 
-function SparkLine() {
+function SparkLine({ id, height, width, color, data, type, currentColor }) {
     return (
-        <div>
-
-        </div>
+        <SparklineComponent
+            id={id}
+            type={type}
+            height={height}
+            width={width}
+            fill={color}
+            dataSource={data}
+            lineWidth={1}
+            valueType='Numeric'
+            border={{ color: 'blue', width: 2 }}
+            xName='x'
+            yName='yval'
+            tooltipSettings={{
+                visible: true,
+                // eslint-disable-next-line no-template-curly-in-string
+                format: '${x} : data ${yval}',
+                trackLineSettings: {
+                    visible: true,
+                },
+            }}
+            markerSettings={{ visible: ['All'], size: 2.5, fill: currentColor }}
+        >
+            <Inject services={[SparklineTooltip]} /> 
+        </SparklineComponent>
     )
 }
 

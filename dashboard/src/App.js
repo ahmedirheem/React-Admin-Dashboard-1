73ae94db45d21ee675409263c548ba42c3a381cb
ahmedirheem <ahmedirheem2003@gmail.com>
2023-01-18 12:14:07 +0200

    Apps

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {FiSettings} from 'react-icons/fi'
+import { FiSettings } from 'react-icons/fi'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
@@ -11,17 +11,17 @@ import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Ka
 import { useStateContext } from './contexts/ContextProvider';
 
 function App() {
-  const {activeMenu} = useStateContext();
+  const { activeMenu } = useStateContext();
 
   return (
     <div className="App">
       <BrowserRouter>
         <div className='flex relative dark:bg-main-dark-bg'>
           {/* Stting Icon */}
-          <div className='fixed right-4 bottom-4' style={{zIndex: '1000'}}>
+          <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
             <TooltipComponent content='Setting' position='Top'>
-              <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{background: 'blue', borderRadius: '50%'}}>
-                <FiSettings/>
+              <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{ background: 'blue', borderRadius: '50%' }}>
+                <FiSettings />
               </button>
             </TooltipComponent>
           </div>
@@ -43,34 +43,34 @@ function App() {
               <Navbar />
             </div>
 
-              <Routes>
-                {/* Dashboard */}
-                <Route path='/' element={<Ecommerce/>}/>
-                <Route path='/ecommerce' element={<Ecommerce/>}/>
+            <Routes>
+              {/* Dashboard */}
+              <Route path='/' element={<Ecommerce />} />
+              <Route path='/ecommerce' element={<Ecommerce />} />
 
-                {/* Pages */}
-                <Route path='/orders' element={<Orders/>}/>
-                <Route path='/employees' element={<Employees/>}/>
-                <Route path='/customers' element={<Customers/>}/>
-                
-                {/* Apps */}
-                <Route path='/calender' element={<Calendar/>}/>
-                <Route path='/kanban' element={<Kanban/>}/>
-                <Route path='/editor' element={<Editor/>}/>
-                <Route path='/color-piker' element={<ColorPicker/>}/>
-                
-                {/* Charts */}
-                <Route path='/line' element={<Line/>}/>
-                <Route path='/area' element={<Area/>}/>
-                <Route path='/bar' element={<Bar/>}/>
-                <Route path='/pie' element={<Pie/>}/>
-                <Route path='/financial' element={<Financial/>}/>
-                <Route path='/color-mapping' element={<ColorMapping/>}/>
-                <Route path='/stacked' element={<Stacked />}/>
-                
-              </Routes>
-            </div>
+              {/* Pages */}
+              <Route path='/orders' element={<Orders />} />
+              <Route path='/employees' element={<Employees />} />
+              <Route path='/customers' element={<Customers />} />
+
+              {/* Apps */}
+              <Route path='/calendar' element={<Calendar />} />
+              <Route path='/kanban' element={<Kanban />} />
+              <Route path='/editor' element={<Editor />} />
+              <Route path='/color-picker' element={<ColorPicker />} />
+
+              {/* Charts */}
+              <Route path='/line' element={<Line />} />
+              <Route path='/area' element={<Area />} />
+              <Route path='/bar' element={<Bar />} />
+              <Route path='/pie' element={<Pie />} />
+              <Route path='/financial' element={<Financial />} />
+              <Route path='/color-mapping' element={<ColorMapping />} />
+              <Route path='/stacked' element={<Stacked />} />
+
+            </Routes>
           </div>
+        </div>
       </BrowserRouter>
     </div>
   );

667eb56e7071734c9e89afc47c314cc34c9383c4
ahmedirheem <ahmedirheem2003@gmail.com>
2023-01-18 11:03:36 +0200

    Pages

--- dashboard/src/components/Header.jsx
+++ dashboard/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-function Header() {
+function Header({category, title}) {
     return (
-        <div>
-
+        <div className='mb-10'> 
+            <p className='text-lg text-gray-400'>{category}</p>
+            <p className='text-3xl text-slate-900 tracking-tight font-extrabold'>{title}</p>
         </div>
     )
 }
--- dashboard/src/pages/Customers.jsx
+++ dashboard/src/pages/Customers.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
+
+import { customersData, contextMenuItems, customersGrid } from '../data/dummy';
+import {Header} from '../components'
 
 function Customers() {
     return (
-        <div>
+        <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+            <Header category='Page' title='Customers' />
 
+            <GridComponent
+                id='gridcomp'
+                dataSource={customersData}
+                enableHover={false}
+                allowPaging
+                pageSettings={{pageCount: 5}}
+                allowSorting
+                selectionSettings={{persistSelection: true}}
+                editSettings={{allowDeleting: true, allowEditing: true}}
+                toolbar={['Delete']}
+            >
+                <ColumnsDirective>
+                    {customersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+                </ColumnsDirective>
+
+                <Inject services={[Sort, Filter, Page, Edit, Selection, Toolbar]} />
+            </GridComponent>
         </div>
     )
 }
